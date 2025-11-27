511d3c878e7dc08c0cc43f47ce3db45eb22bdadb
Sabbir Ahmed <imsabbiir@gmail.com>
2025-11-27 09:24:55 +0600

    footer fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='flex justify-between boxBg subTitleText py-4 text-xs px-5'>
+    <div className='flex justify-between boxBg subTitleText py-4 text-xs px-5 mb-7'>
         <h3 className='hidden md:block'>© 2024 Sabbir Ahmed. All rights reserved.</h3>
         <h3>Template authar: <span className='activeText'>Sabbir Ahmed</span></h3>
     </div>
--- src/Components/Layouts/Main/Children.jsx
+++ src/Components/Layouts/Main/Children.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Footer from "@/Components/Footer";
 import React from "react";
 import { Scrollbar } from "smooth-scrollbar-react";
@@ -18,7 +18,7 @@ function Children({ children }) {
       continuousScrolling={true}
     >
       <div
-        className={`w-full lg:w-[calc(100%-370px)] pb-16 lg:pb-0 lg:mr-[82px] relative transition-all duration-300 overflow-y-auto children`}
+        className={`w-full lg:w-[calc(100%-370px)] h-screen bg-red-400 pb-16 lg:pb-0 lg:mr-[82px] relative transition-all duration-300 overflow-y-auto children`}
       >
         <div className="bg-[#000000a4] w-full h-full absolute top-0 left-0 z-50 opacity-0 hidden childrenOverlay transition-all duration-300 ease-in-out"></div>
 
