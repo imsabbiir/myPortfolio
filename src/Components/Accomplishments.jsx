756c26319fa935193345d68ecb038ba3c7911134
Sabbir Ahmed <imsabbiir@gmail.com>
2026-04-26 08:56:30 +0600

    solve public url problem

--- src/Components/Accomplishments.jsx
+++ src/Components/Accomplishments.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 async function Accomplishments() {
-  const res = await fetch("http://localhost:3000/api/projectCount", {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/projectCount`, {
     cache: "no-store",
   });
   const data = await res.json();
